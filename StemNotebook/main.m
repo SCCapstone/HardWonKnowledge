0aecf043e09c9afb1039457edbe6ca918cf46020
Colton Waters <coltonwaters92@email.sc.edu>
2013-10-25 17:03:02 -0400

    Initial Commit

---
+++ StemNotebook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StemNotebook
+//
+//  Created by Colton Waters on 10/25/13.
+//  Copyright (c) 2013 HardWonKnowledge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
