0ae49d812aa8db76154bd6ec45b6fb03c0b66de2
Colton Waters <coltonwaters92@email.sc.edu>
2013-10-27 17:05:57 -0400

    Initial Commit

---
+++ StemNotebook/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  StemNotebook
+//
+//  Created by Colton Waters on 10/27/13.
+//  Copyright (c) 2013 HardWonKnowledge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
