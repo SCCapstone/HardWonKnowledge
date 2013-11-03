6b65c8adc213babd337d088271aec3ea843af001
Colton Waters <coltonwaters92@email.sc.edu>
2013-11-03 15:21:30 -0500

    PaintView is added. SideBarView is added. Nothing is in sidebar view, just grey background. Views are sized correctly. There is a view in the ViewController.xib for the submenu views, but those will be added to the window on an as-needed basis (i.e.. When the corresponding sidebar button is pressed).

---
+++ StemNotebook/SideBarView.h
@@ -0,0 +1,13 @@
+//
+//  testView.h
+//  StemNotebook
+//
+//  Created by Colton Waters on 11/1/13.
+//  Copyright (c) 2013 HardWonKnowledge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideBarView: UIView
+
+@end
