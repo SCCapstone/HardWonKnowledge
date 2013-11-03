751f2f273d14ab9e87bdd40017cb4dbb3acf211a
jmwood <jacob@Jacobs-Mac.local>
2013-11-03 16:47:35 -0500

    added new subview
    doesnt do anything.  Still need to figure out how it works

---
+++ StemNotebook/PaintSubmenuView.m
@@ -0,0 +1,31 @@
+//
+//  PaintSubmenuView.m
+//  StemNotebook
+//
+//  Created by Jacob on 11/3/13.
+//  Copyright (c) 2013 HardWonKnowledge. All rights reserved.
+//
+
+#import "PaintSubmenuView.h"
+
+@implementation PaintSubmenuView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
