7cccba440aa44100411d5be50ea31ca1e5d0ae02
jmwood <jacob@Jacobs-Mac.local>
2013-11-03 16:47:35 -0500

    added new subview
    doesnt do anything.  Still need to figure out how it works

---
+++ StemNotebook/PaintSubmenuView.h
@@ -0,0 +1,13 @@
+//
+//  PaintSubmenuView.h
+//  StemNotebook
+//
+//  Created by Jacob on 11/3/13.
+//  Copyright (c) 2013 HardWonKnowledge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaintSubmenuView : UIView
+
+@end
