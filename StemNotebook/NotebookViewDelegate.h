d37511f63bfe13612d55f6df33bfabd3d18ae7d2
Colton Waters <coltonwaters92@email.sc.edu>
2013-12-02 13:49:18 -0500

    Not sure why this didn’t commit

--- StemNotebook/NotebookViewDelegate.h
+++ StemNotebook/NotebookViewDelegate.h
@@ -13,5 +13,6 @@
 - (void)PaintViewButtonPressed;
 - (void)changeColorWithRed:(float)newRed Blue:(float)newBlue Green:(float)newGreen;
 - (void)changeBrushWithNumber:(float)number;
+- (void)showPaintSubmenu;
 
 @end
