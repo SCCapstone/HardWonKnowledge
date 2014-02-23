f9b6827d52ca1f4df1f960b8cc04b520c99a367a
Saraswati <2010kbrown@gmail.com>
2014-02-23 16:22:51 -0500

    Basic bookshelf view. Login screen.

---
+++ Bookshelf/Classes/UserLoginViewController.h
@@ -0,0 +1,23 @@
+//
+//  UserLoginViewController.h
+//  Bookshelf
+//
+//  Created by Keneequa Brown on 2/20/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserLoginViewController : UIViewController{
+    IBOutlet UITextField *usernameField;
+    IBOutlet UITextField *passwordField;
+    NSMutableArray *usernameCredentials;
+    NSMutableArray *passwordCredentials;
+//    NSDictionary *userCredentials;
+    NSMutableDictionary *userCredentials;
+    
+}
+//- (IBAction)userLogin:(id)sender;
+//@property (strong, nonatomic) IBOutlet UILabel *ButtonPressedNameLabel;
+- (IBAction)userLogin;
+@end
---
+++ Bookshelf/Classes/UserLoginViewController.m
@@ -0,0 +1,56 @@
+//
+//  UserLoginViewController.m
+//  Bookshelf
+//
+//  Created by Keneequa Brown on 2/20/14.
+//
+//
+
+#import "UserLoginViewController.h"
+#import "BookshelfGridViewController.h"
+
+@interface UserLoginViewController ()
+
+@end
+
+@implementation UserLoginViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"users" ofType:@"txt"];
+    NSString *fileContents = [NSString stringWithContentsOfFile:filePath];
+    NSArray* rows = [fileContents componentsSeparatedByString:@"\n"];
+    
+    userCredentials = [[NSMutableDictionary alloc] init];
+    for (int n=0; n<[rows count]; n++){
+        NSString* users = [rows objectAtIndex:n];
+        NSArray* userInfo = [users componentsSeparatedByString:@" "];
+        
+        NSString *theUsername = [userInfo objectAtIndex:0];
+        theUsername= [theUsername stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        theUsername = [theUsername lowercaseString];
+        [usernameCredentials addObject:theUsername];
+        NSString *thePassword = [userInfo objectAtIndex:1];
+        thePassword = [thePassword stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        [passwordCredentials addObject:thePassword];
+        [userCredentials setObject:thePassword forKey:theUsername];
+    }
+}
+
+- (IBAction)userLogin{
+    if([[userCredentials objectForKey:@"admin"]isEqualToString:passwordField.text]){
+            // Sign in.
+    }
+    else if([[userCredentials objectForKey:usernameField.text]isEqualToString:passwordField.text]){
+        BookshelfGridViewController *bookshelf = [[BookshelfGridViewController alloc] initWithNibName:nil bundle:nil];
+        [self presentViewController:bookshelf animated:YES completion:NULL];
+    }
+    else{
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Incorrect Password" message:@"Please enter a correct password/username combination." delegate:self cancelButtonTitle:@"Dismiss" otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+@end
