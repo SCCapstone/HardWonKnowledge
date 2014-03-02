8281b7209d215cb1959b0ca2b65a078af99bcd2d
Saraswati <2010kbrown@gmail.com>
2014-03-01 22:03:17 -0500

    Added more methods to clean code a bit.

--- HardWonKnowledge/Bookshelf/Classes/UserLoginViewController.h
+++ HardWonKnowledge/Bookshelf/Classes/UserLoginViewController.h
@@ -10,17 +10,27 @@
 #import "GTLDrive.h"
 #import "DriveManager.h"
 
-@interface UserLoginViewController : UIViewController<UIAlertViewDelegate>
+@interface UserLoginViewController : UIViewController<UIAlertViewDelegate, UITableViewDelegate, UITableViewDataSource>
 
-- (IBAction)userLogin;
+- (void)parseFile: (NSArray *)rows;
 - (IBAction)driveLogin;
 - (IBAction)driveLogout;
+- (IBAction)closeAnimated;
+- (IBAction)closePlain;
+- (void)openView: (NSString*)title value:(BOOL)value;
+- (UIButton*)addButton: (NSString*)title x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height;
+- (void)addLabel: (NSString*)title x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height color:(UIColor*)color alignment:(UITextAlignment)align fontSize:(CGFloat)size lines:(NSInteger)numberOfLines;
+-(UITableView*)addAutoComplete;
+- (UITextField*)addTextField: (NSString*)placeholder x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height fontSize:(CGFloat)size secure:(BOOL)value capitalize:(BOOL)cap;
+- (IBAction)userLogin;
+- (IBAction)adminMenu;
 - (IBAction)userSettings;
-- (IBAction)closeView;
 - (IBAction)addUser;
+- (IBAction)editUser;
 - (IBAction)viewUser;
 - (IBAction)submitUser;
-- (IBAction)editUser;
+- (void)uploadListFile: (NSString*)filePath exists:(BOOL)value;
+
 @property (weak, nonatomic) IBOutlet UIButton *LogInButton;
 @property (nonatomic, retain) GTLServiceDrive *driveService;
 @property (nonatomic, retain) DriveManager *driveManager;
