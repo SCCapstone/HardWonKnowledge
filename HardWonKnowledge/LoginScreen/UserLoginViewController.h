820f40d039ac8da136b7be06c24275077dbdbd1b
Saraswati <2010kbrown@gmail.com>
2014-03-11 01:19:59 -0400

    Split files. User login view.

---
+++ HardWonKnowledge/LoginScreen/UserLoginViewController.h
@@ -0,0 +1,19 @@
+//
+//  UserLoginViewController.h
+//  StemNotebook
+//
+//  Created by Keneequa Brown on 2/20/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "UserLoginBackend.h"
+#import "AdminView.h"
+
+@interface UserLoginViewController : UIViewController<UIAlertViewDelegate>   
+
+- (IBAction)menuLoginScreen;
+
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (nonatomic, retain) AdminView *adminView;
+@end
