a448be852ce10eecb2dc323b1ad01279fedf9e94
Saraswati <2010kbrown@gmail.com>
2014-03-11 01:54:52 -0400

    Updated imported header location.

--- HardWonKnowledge/StemNotebook/AppDelegate.h
+++ HardWonKnowledge/StemNotebook/AppDelegate.h
@@ -8,7 +8,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "../Bookshelf/Classes/UserLoginViewController.h"
+#import "../LoginScreen/UserLoginViewController.h"
 @class UserLoginViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
