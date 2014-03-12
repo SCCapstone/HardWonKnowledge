c86006b3cd2e282d01ed99998430c0cbf24ac50c
Saraswati <2010kbrown@gmail.com>
2014-03-11 21:47:19 -0400

    Added more methods.

--- HardWonKnowledge/LoginScreen/AdminView.h
+++ HardWonKnowledge/LoginScreen/AdminView.h
@@ -20,15 +20,23 @@
 }
 
 - (UIButton*)addButton: (NSString*)title x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height;
-- (void)addLabel: (NSString*)title x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height color:(UIColor*)color alignment:(UITextAlignment)align fontSize:(CGFloat)size bold:(BOOL)bold;
+- (void)addLabel: (NSString*)title x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height color:(UIColor*)color alignment:(UITextAlignment)align fontSize:(CGFloat)size isBold:(BOOL)bold;
+- (void)addSwitch;
+- (UITextField*)addTextField: (NSString*)placeholder x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height fontSize:(CGFloat)size secure:(BOOL)value capitalize:(BOOL)cap;
+- (void)addTextView;
+- (void)alertOneButton: (NSString*)title message:(NSString*)mssg buttonTitle:(NSString*)btn;
+- (void)alertTwoButtons: (NSString*)title message:(NSString*)mssg firstButton:(NSString*)btn1 secondButton:(NSString*)btn2;
+- (void)clearScreen;
 - (IBAction)confirmUser;
-- (IBAction)menuAdminAdd;
+- (void)menuAdminAdd;
 - (IBAction)menuAdminEdit;
 - (IBAction)menuAdminSettings;
 - (void)menuAdminUpdate;
 - (void)openView: (NSString*)title;
 - (IBAction)promptRemoveUser;
 - (IBAction)promptUpdateUser;
+//tap close keyboard
+//dismisskeyboard to no
 
 @property (nonatomic, retain) UserLoginBackend *loginBackend;
 @property (nonatomic,retain) NSMutableArray *subviews;
