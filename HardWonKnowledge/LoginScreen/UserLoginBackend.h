5ad4791c34d5d0f5435b32eb5b8ac1a99e2f612b
Saraswati <2010kbrown@gmail.com>
2014-03-14 01:23:31 -0400

    Cleaned code.

--- HardWonKnowledge/LoginScreen/UserLoginBackend.h
+++ HardWonKnowledge/LoginScreen/UserLoginBackend.h
@@ -11,22 +11,13 @@
 #import "DriveManager.h"
 
 @interface UserLoginBackend : NSObject
-//    IBOutlet UITextField *data1;
-//    IBOutlet UITextField *data2;
-//    IBOutlet UITextField *data3;
-//    IBOutlet UITextField *data4;
-//    UIViewController *adminView;
-//    UITableView *myTableView;
-//    NSMutableArray *dataSrc; //will be storing all the data
-//    NSMutableArray *tblData;//will be storing data that will be displayed in table
-//    NSMutableArray *srchedData;//will be storing data matching with the search string
-//    UISearchBar *sBar;//search bar
+
+- (BOOL)isAdminUser: (NSString *)name;
+- (BOOL)isStudentUser: (NSString *)name;
 
 - (void)findDriveData;
 - (void)findBundleData;
 - (void)initVariables;
-- (BOOL)isAdminUser: (NSString *)name;
-- (BOOL)isStudentUser: (NSString *)name;
 - (void)parseText: (NSString *)text file:(NSInteger)fileType;
 - (void)resetUsers;
 - (void)removeSelectedUser: (NSString *)username;
