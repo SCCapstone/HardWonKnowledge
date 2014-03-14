e8c4f8921563fb33429cce292f835c7805d9fdb2
Saraswati <2010kbrown@gmail.com>
2014-03-13 22:33:07 -0400

    Added index values to UIView methods, button method now includes the target action, changed a method name, and exchanged 5 UITextField variables for one array.

--- HardWonKnowledge/LoginScreen/AdminView.h
+++ HardWonKnowledge/LoginScreen/AdminView.h
@@ -11,39 +11,42 @@
 #import "BookshelfGridViewController.h"
 
 @interface AdminView : UIViewController <UIAlertViewDelegate, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UITextFieldDelegate, UITextViewDelegate>{
-    UITextField *tf0;
-    UITextField *tf1;
-    UITextField *tf2;
-    UITextField *tf3;
-    UITextField *tf4;
+//    UITextField *tf0;
+//    UITextField *tf1;
+//    UITextField *tf2;
+//    UITextField *tf3;
+//    UITextField *tf4;
     BOOL isAdmin;
 }
 
-- (UIButton*)addButton: (NSString*)title x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height;
-- (UITextField*)addTextField: (NSString*)placeholder x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height fontSize:(CGFloat)size secure:(BOOL)value capitalize:(BOOL)cap;
-- (void)addTextView: (NSString*)text x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height fontSize:(CGFloat)size;
+- (void)addButton: (NSInteger)index title:(NSString*)title action:(SEL)action x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height;
+- (void)addTextField: (NSInteger)index placeholder:(NSString*)placeholder x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height fontSize:(CGFloat)size secure:(BOOL)value capitalize:(BOOL)cap;
+- (void)addTextView: (NSInteger)index text:(NSString*)text x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height fontSize:(CGFloat)size;
 
-- (void)addLabel: (NSString*)title x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height color:(UIColor*)color alignment:(UITextAlignment)align fontSize:(CGFloat)size isBold:(BOOL)bold;
-- (void)addSwitch: (BOOL)useYes x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height;
+- (void)addLabel: (NSInteger)index title:(NSString*)title x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height color:(UIColor*)color alignment:(UITextAlignment)align fontSize:(CGFloat)size isBold:(BOOL)bold;
+- (void)addSwitch: (NSInteger)index isOn:(BOOL)isOn x:(CGFloat)x y:(CGFloat)y width:(CGFloat)width height:(CGFloat)height;
 - (void)alertOneButton: (NSString*)title message:(NSString*)mssg buttonTitle:(NSString*)btn;
 - (void)alertTwoButtons: (NSString*)title message:(NSString*)mssg firstButton:(NSString*)btn1 secondButton:(NSString*)btn2;
 - (void)changeSwitch:(id)sender;
 - (void)clearScreen;
 - (void)menuAdminAdd;
-- (IBAction)menuAdminUpdate;
 - (void)openView: (NSString*)title;
 - (void)configUpdateUser;
+- (void)submitAddedUser;
+- (void)submitUpdatedUser;
 
 - (IBAction)closeView;
-- (IBAction)confirmUser;
+- (IBAction)confirmUserInsertion;
 - (IBAction)menuAdminEdit;
 - (IBAction)menuAdminSettings;
+- (IBAction)menuAdminUpdate;
+- (IBAction)promptAddUser;
 - (IBAction)promptRemoveUser;
 - (IBAction)promptUpdateUser;
 
 @property (nonatomic, retain) UserLoginBackend *loginBackend;
 @property (nonatomic,retain) NSMutableArray *subviews;
-//@property (nonatomic,retain) NSMutableArray *texts;
+@property (nonatomic,retain) NSMutableArray *savedText;
 @property (nonatomic,retain) NSMutableArray *srchedData;
 @property (nonatomic,retain) NSMutableArray *tblData;
 @property (nonatomic,retain) UISearchBar *sBar;
