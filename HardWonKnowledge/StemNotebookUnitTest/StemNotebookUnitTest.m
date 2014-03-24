7e18705e9942f5c63c4664c2438eeff1b5023619
JACOB MICHAEL WOOD <woodjm2@pear.cse.sc.edu>
2014-03-24 17:05:07 -0400

    Added unit testing to application

---
+++ HardWonKnowledge/StemNotebookUnitTest/StemNotebookUnitTest.m
@@ -0,0 +1,32 @@
+//
+//  StemNotebookUnitTest.m
+//  StemNotebookUnitTest
+//
+//  Created by JACOB MICHAEL WOOD on 3/24/14.
+//  Copyright (c) 2014 HardWonKnowledge. All rights reserved.
+//
+
+#import "StemNotebookUnitTest.h"
+
+@implementation StemNotebookUnitTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in StemNotebookUnitTest");
+}
+
+@end
