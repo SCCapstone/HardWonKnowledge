4fa0a179591b11ab4f8b1ccf9c187c7755aa3fd4
KENEEQUA CHONTESE BROWN <brownkc2@grape.cse.sc.edu>
2014-03-24 18:31:18 -0400

    LoginHeader

---
+++ HardWonKnowledge/StemNotebookUnitTest/UserLoginTest.h
@@ -0,0 +1,13 @@
+//
+//  UserLoginTest.h
+//  StemNotebook
+//
+//  Created by KENEEQUA CHONTESE BROWN on 3/24/14.
+//  Copyright (c) 2014 HardWonKnowledge. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UserLoginTest : SenTestCase
+
+@end
