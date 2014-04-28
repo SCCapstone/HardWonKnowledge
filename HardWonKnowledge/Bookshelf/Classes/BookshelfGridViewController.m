3654e148252fa9ddd91ce0ba6942f99ee3b3e10a
Keneequa Brown <2010kbrown@gmail.com>
2014-04-28 19:25:47 -0400

    renamed notebooks in code

--- HardWonKnowledge/Bookshelf/Classes/BookshelfGridViewController.m
+++ HardWonKnowledge/Bookshelf/Classes/BookshelfGridViewController.m
@@ -266,9 +266,9 @@ enum
     else if(viewingAdmin == 1)
         filledCell.image = [UIImage imageNamed:@"folder.png"];
     else if(index<=[_driveTitles count])
-        filledCell.image = [UIImage imageNamed:@"green_notebook_wide.png"];
+        filledCell.image = [UIImage imageNamed:@"green_notebook"];
     else
-        filledCell.image = [UIImage imageNamed:@"black_notebook_wide.png"];
+        filledCell.image = [UIImage imageNamed:@"black_notebook"];
     
     filledCell.title = [_allNotebooks objectAtIndex:index];
     
