49aab201e079d7a5108cfa2f9d60fb53041ee7a1
Keneequa Brown <2010kbrown@gmail.com>
2014-04-28 18:46:07 -0400

    Unit Testing

--- HardWonKnowledge/StemNotebookUnitTest/UserLoginTest.h
+++ HardWonKnowledge/StemNotebookUnitTest/UserLoginTest.h
@@ -2,7 +2,7 @@
 //  UserLoginTest.h
 //  StemNotebook
 //
-//  Created by KENEEQUA CHONTESE BROWN on 3/24/14.
+//  Created by KENEEQUA BROWN on 3/24/14.
 //  Copyright (c) 2014 HardWonKnowledge. All rights reserved.
 //
 
--- HardWonKnowledge/StemNotebookUnitTest/UserLoginTest.m
+++ HardWonKnowledge/StemNotebookUnitTest/UserLoginTest.m
@@ -2,7 +2,7 @@
 //  UserLoginTest.m
 //  StemNotebook
 //
-//  Created by KENEEQUA CHONTESE BROWN on 3/24/14.
+//  Created by KENEEQUA BROWN on 3/24/14.
 //  Copyright (c) 2014 HardWonKnowledge. All rights reserved.
 //
 
@@ -11,9 +11,33 @@
 
 @implementation UserLoginTest
 
-- (void)testBlankLogin{
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testOpenUserLogin {
     UserLoginViewController *userLogin = [[UserLoginViewController alloc]init];
-    STAssertNotNil(userLogin, @"")
+    STAssertNotNil(userLogin, @"Open of UserLoginViewController is nil");
+}
+
+- (void)testInstantiation {
+    UserLoginViewController *userLogin = [[UserLoginViewController alloc]init];
+    STAssertNotNil(userLogin, @"Test instance of UserLoginViewController is nil");
+}
+
+- (void)testSharedInstanceNotNil {
+    UserLoginViewController *userLogin = [UserLoginViewController sharedInstance];
+    STAssertNotNil(userLogin, @"sharedInstance of UserLoginViewController is nil");
+}
+
+- (void)testSharedInstanceReturnsSameSingletonObject {
+    UserLoginViewController *userLogin1 = [UserLoginViewController sharedInstance];
+    UserLoginViewController *userLogin2 = [UserLoginViewController sharedInstance];
+    STAssertEquals(userLogin1, userLogin2, @"sharedInstance of UserLoginViewController didn't return same object twice");
 }
 
 @end
